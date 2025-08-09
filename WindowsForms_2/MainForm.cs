04a3f2afd758aee58ce658f77a16f8c10ebb9ff1
Veronika050987 <veronicaviv@mail.ru>
2025-08-09 14:42:43 +0300

    main menu in progress

--- WindowsForms_2/MainForm.cs
+++ WindowsForms_2/MainForm.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace WindowsForms_2
 {
@@ -16,6 +17,7 @@ namespace WindowsForms_2
 		public MainForm()
 		{
 			InitializeComponent();
+			ShowControls(cmShowControls.Checked);
 		}
 
 		void ShowControls(bool visible)
@@ -27,6 +29,16 @@ namespace WindowsForms_2
 			this.TransparencyKey = visible ? Color.Empty : this.BackColor;
 			this.FormBorderStyle = visible ? FormBorderStyle.FixedDialog : FormBorderStyle.None;
 			this.labelCurrentTime.BackColor = visible ? this.BackColor : Color.DeepSkyBlue;
+		}
+
+		void ShowConsole(bool visible)
+		{
+			//bool console = visible ? AllocConsole() : FreeConsole();
+			//if(console)Console.WriteLine(console);
+			if (visible)
+				AllocConsole();
+			else
+				FreeConsole();
 		}
 
 		private void timer_Tick(object sender, EventArgs e)
@@ -41,16 +53,18 @@ namespace WindowsForms_2
 				labelCurrentTime.Text += $"\n{DateTime.Now.DayOfWeek}";
 
 			notifyIcon.Text = labelCurrentTime.Text;
+			if(cmDebugConsole.Checked)
+				Console.WriteLine(notifyIcon.Text);
 		}
 
 		private void btnHideControls_Click(object sender, EventArgs e)
 		{
-			ShowControls(false);
+			ShowControls(cmShowControls.Checked = false);
 		}
 
 		private void labelCurrentTime_DoubleClick(object sender, EventArgs e)
 		{
-			ShowControls(true);
+			ShowControls(cmShowControls.Checked = true);
 		}
 
 		private void cmClose_Click(object sender, EventArgs e)
@@ -58,5 +72,32 @@ namespace WindowsForms_2
 			this.Close();
 		}
 
+		private void cmTopmost_CheckedChanged(object sender, EventArgs e)
+		{
+			this.TopMost = cmTopmost.Checked;
+		}
+
+		private void cmShowControls_CheckedChanged(object sender, EventArgs e)
+		{
+			ShowControls(cmShowControls.Checked);
+		}
+
+		////////////////////////////////////
+		[DllImport("kernel32.dll")]
+		public static extern bool AllocConsole();
+		[DllImport("kernel32.dll")]
+		public static extern bool FreeConsole();
+
+		private void cmDebugConsole_CheckedChanged(object sender, EventArgs e)
+		{
+			ShowConsole(cmDebugConsole.Checked);
+		}
+
+		private void notifyIcon_DoubleClick(object sender, EventArgs e)
+		{
+			if (this.TopMost) return;
+			this.TopMost = true;
+			this.TopMost = false;
+		}
 	}
 }
